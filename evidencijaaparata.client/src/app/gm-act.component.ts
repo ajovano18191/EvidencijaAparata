dffeded42553323f1216784df7dd486259d20a28
ajovano <aleksa.jovanovicc@elfak.rs>
2024-08-31 19:54:16 +0200

    GM_Act komponenta

---
+++ evidencijaaparata.client/src/app/gm-act.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gm-act',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './gm-act.component.html',
+  styleUrls: ['./gm-act.component.css']
+})
+export class GmActComponent {
+
+}
