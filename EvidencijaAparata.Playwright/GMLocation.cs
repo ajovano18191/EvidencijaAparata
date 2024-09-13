c6317fdf0fc8f2327726fded49348370f90b6480
ajovano <aleksa.jovanovicc@elfak.rs>
2024-09-13 18:40:23 +0200

    GMBase crud, activate, deactivate best case

--- EvidencijaAparata.Playwright/GMLocation.cs
+++ EvidencijaAparata.Playwright/GMLocation.cs
@@ -3,7 +3,6 @@ using Microsoft.Playwright;
 
 namespace EvidencijaAparata.Playwright
 {
-    // [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class GMLocationTest : PageTest
     {
@@ -16,27 +15,30 @@ namespace EvidencijaAparata.Playwright
             browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
                 Headless = false,
-                SlowMo = 2000,
-
+                SlowMo = 1000,
             });
 
             page = await browser.NewPageAsync();
+            await page.GotoAsync("http://localhost:4200");
+        }
+
+        [TearDown]
+        public async Task TeardownAsync()
+        {
+            await browser.CloseAsync();
         }
 
         [Test]
         [Order(0)]
         public async Task GetTest()
         {
-            await page.GotoAsync("http://localhost:4200");
-            // await Expect(Page.Locator("circle").Nth(1)).Not.ToBeVisibleAsync();
-            // await browser.CloseAsync();
+            await Expect(page.Locator("circle").Nth(1)).Not.ToBeVisibleAsync();
         }
 
         [Test]
         [Order(1)]
         public async Task AddGMLocation()
         {
-            await page.GotoAsync("http://localhost:4200");
             await page.GetByLabel("Add", new() { Exact = true }).ClickAsync();
             await Expect(page.Locator(".cdk-overlay-backdrop")).ToBeVisibleAsync();
             await page.GetByLabel("Rul. Base ID").FillAsync("100");
@@ -46,16 +48,13 @@ namespace EvidencijaAparata.Playwright
             await page.GetByLabel("Mesto").Locator("span").ClickAsync();
             await page.GetByRole(AriaRole.Option, new() { Name = "City 1", Exact = true }).ClickAsync();
             await page.GetByRole(AriaRole.Button, new() { Name = "Add", Exact = true }).ClickAsync();
-            await Expect(page.GetByRole(AriaRole.Cell, new() { Name = "100", Exact = true })).ToBeVisibleAsync(); // Exact = true
-            await browser.CloseAsync();
+            await Expect(page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") })).ToBeVisibleAsync();
         }
 
         [Test]
         [Order(2)]
         public async Task EditGMLocation()
         {
-            await page.GotoAsync("http://localhost:4200");
-            Thread.Sleep(2000);
             await page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Edit").ClickAsync();
             await Expect(page.Locator(".cdk-overlay-backdrop")).ToBeVisibleAsync();
             await page.GetByLabel("Name").FillAsync("Probaaa");
@@ -66,15 +65,12 @@ namespace EvidencijaAparata.Playwright
             await Expect(page.Locator("tbody")).ToContainTextAsync("Probaaa");
             await Expect(page.Locator("tbody")).ToContainTextAsync("Adresaaa");
             await Expect(page.Locator("tbody")).ToContainTextAsync("City 2");
-            await browser.CloseAsync();
         }
 
         [Test]
         [Order(3)]
         public async Task ActivateGMLocation()
         {
-            await page.GotoAsync("http://localhost:4200");
-            Thread.Sleep(2000);
             await page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Deactive").ClickAsync();
             await Expect(page.Locator(".cdk-overlay-backdrop")).ToBeVisibleAsync();
             await page.GetByLabel("Resenje").FillAsync("ResAkt");
@@ -83,33 +79,26 @@ namespace EvidencijaAparata.Playwright
             await page.GetByLabel("Napomena").FillAsync("NapomenaAkt");
             await page.GetByRole(AriaRole.Button, new() { Name = "Activate", Exact = true }).ClickAsync();
             await Expect(page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Active")).ToHaveCountAsync(1);
-            await browser.CloseAsync();
         }
 
         [Test]
         [Order(4)]
         public async Task DeactivateGMLocation()
         {
-            await page.GotoAsync("http://localhost:4200");
-            Thread.Sleep(2000);
             await page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Active").ClickAsync();
             await page.GetByLabel("Resenje").FillAsync("ResenjeDeakt");
             await page.GetByLabel("Open calendar").ClickAsync();
             await page.GetByLabel("12. септембар").ClickAsync();
             await page.GetByRole(AriaRole.Button, new() { Name = "Deactivate", Exact = true }).ClickAsync();
             await Expect(page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Deactive")).ToHaveCountAsync(1);
-            await browser.CloseAsync();
         }
 
         [Test]
         [Order(5)]
         public async Task DeleteGMLocation()
         {
-            await page.GotoAsync("http://localhost:4200");
-            Thread.Sleep(2000);
             await page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") }).GetByLabel("Delete").ClickAsync();
             await Expect(page.GetByRole(AriaRole.Row, new() { NameRegex = new Regex(".* 100") })).ToHaveCountAsync(0);
-            await browser.CloseAsync();
         }
     }
 }
