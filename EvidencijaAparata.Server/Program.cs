b05d62847e60c91801aa16ddba3ef50a4c7e32d6
ajovano <aleksa.jovanovicc@elfak.rs>
2024-10-18 17:39:26 +0200

    Dodat http loging

--- EvidencijaAparata.Server/Program.cs
+++ EvidencijaAparata.Server/Program.cs
@@ -13,11 +13,13 @@ builder.Services.AddSwaggerGen();
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //     options.UseInMemoryDatabase("TestDatabase"));
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PGTest")).EnableSensitiveDataLogging());
 
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowAll", builder => {
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
         builder
             .AllowAnyOrigin()
             .AllowAnyMethod()
@@ -26,13 +28,16 @@ builder.Services.AddCors(options => {
     });
 });
 
+builder.Services.AddHttpLogging(p => { });
+
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -46,5 +51,6 @@ app.MapControllers();
 app.MapFallbackToFile("/index.html");
 
 app.UseCors("AllowAll");
+app.UseHttpLogging();
 
 app.Run();
